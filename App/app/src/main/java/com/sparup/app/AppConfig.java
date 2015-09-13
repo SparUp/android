8c2d1060a3262cab8cb7973a34086ad9cfee490e
Vivek Gupta <vivekrpg@gmail.com>
2015-09-12 21:43:52 -0700

    Login Activity : with SQL support and volley library
    This communicates with SQL db and uses JSON format.

---
+++ App/app/src/main/java/com/sparup/app/AppConfig.java
@@ -0,0 +1,14 @@
+package com.sparup.app;
+
+/**
+ * Created by vvgupta on 9/12/2015.
+ */
+//package info.androidhive.loginandregistration.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.101/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.101/android_login_api/";
+}
